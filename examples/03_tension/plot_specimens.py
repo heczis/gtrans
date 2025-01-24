2d332e224c48a2386d27c6b8d4f7e21301ccb5fb
Jan Heczko <jheczko@kme.zcu.cz>
2025-01-24 12:40:03 +0100

    Change plots of specimens (example 03)

--- examples/03_tension/plot_specimens.py
+++ examples/03_tension/plot_specimens.py
@@ -2,6 +2,7 @@
 Plot the tensile specimens with different infill patterns
 """
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 import gcode
 
@@ -18,14 +19,19 @@ DEFAULT_PARS = (
      'examples/03_tension/T_04.pdf'),
     ('examples/03_tension/T_04_out_Hilbert_0.2mm_PLA_MK3.gcode',
      'examples/03_tension/T_04_Hilbert.pdf'),
-)[4:]
+)
 
-def plot_file(gcode_file, output_file=None):
-    fig, ax = plt.subplots(
-        figsize=(5, 1.5),
-        layout='constrained'
+def plot_file(gcode_file, output_file=None, **kwargs):
+    fig = plt.figure(
+        figsize=(7, 1.5),
+        layout='constrained',
     )
+    gs = GridSpec(1, 6, figure=fig)
+    ax = fig.add_subplot(gs[0, 0:5])
+    ax_zoom = fig.add_subplot(gs[0, 5])
+
     ax.axis('equal')
+    ax_zoom.axis('equal')
 
     gc = gcode.GCode.from_file(gcode_file)
     end_layer = 2
@@ -39,10 +45,14 @@ def plot_file(gcode_file, output_file=None):
                 break
 
     gc.instructions = gc.instructions[:end_ind]
-    gc.plot(color='#1f77b4', ax=ax)
+    gc.plot(ax=ax, **kwargs)
 
     ax.set(xlim=(85, 165), ylim=(95, 115))
     ax.axis('off')
+
+    gc.plot(ax=ax_zoom, **kwargs)
+    ax_zoom.set(xlim=(95, 100), ylim=(102, 107))
+    ax_zoom.axis('off')
 
     if output_file is not None:
         plt.savefig(output_file)
@@ -50,5 +60,6 @@ def plot_file(gcode_file, output_file=None):
     plt.show()
 
 if __name__ == '__main__':
-    for pars in DEFAULT_PARS:
-        plot_file(*pars)
+    pcyc = plt.rcParams['axes.prop_cycle']
+    for pars, style_kwargs in zip(DEFAULT_PARS, pcyc):
+        plot_file(*pars, **style_kwargs)
